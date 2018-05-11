b27d39f3743616d2fc5ed4d89b58de39efa8f131
Lynix <lynix680@gmail.com>
2018-05-11 23:28:25 +0200

    Fix copyright notices

--- src/Client/ClientApplication.cpp
+++ src/Client/ClientApplication.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 Jérôme Leclercq
-// This file is part of the "Erewhon Shared" project
+// This file is part of the "Erewhon Client" project
 // For conditions of distribution and use, see copyright notice in LICENSE
 
 #include <Client/ClientApplication.hpp>
