b7d0501fd3687414b6dc528f7895cb118e640056
Lynix <lynix680@gmail.com>
2018-05-11 23:31:07 +0200

    Fix copyright notices for server too

--- src/Server/Components/LifeTimeComponent.cpp
+++ src/Server/Components/LifeTimeComponent.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 Jérôme Leclercq
-// This file is part of the "Erewhon Shared" project
+// This file is part of the "Erewhon Server" project
 // For conditions of distribution and use, see copyright notice in LICENSE
 
 #include <Server/Components/LifeTimeComponent.hpp>
